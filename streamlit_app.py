544098afe433c5f3062e4a50f0f8e78c527e0071
panman <ts@tinoMacBook-Air.local>
2023-09-07 20:18:38 +0900

    change path

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,14 +14,14 @@ if uploaded_image is not None:
     st.image(uploaded_image, caption='アップロードされた画像', use_column_width=True)
 
     if st.button("解答を表示"):
-        image = np.array(Image.open(uploaded_image))
+        # image = np.array(Image.open(uploaded_image))
 
         # OpenCVで処理した画像を一時的なファイルに保存
         # with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_image:
         #     temp_image_path = temp_image.name
         #     cv2.imwrite(temp_image_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
         
-        result_image = main(image)
+        result_image = main(uploaded_image)
 
         # 解いた数独を表示
         st.image(result_image, caption='解かれた数独', use_column_width=True)
